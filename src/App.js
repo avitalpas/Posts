a2b9fd2260d47ed299f97d4b3898a2a26aeb39b4
avitalpas <65176812+avitalpas@users.noreply.github.com>
2020-07-14 16:53:20 +0300

    1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hi
+    </div>
+  );
+}
+
+export default App;
